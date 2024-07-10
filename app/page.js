af291f4f5ae7773d7470dab25164bec6c1ddb3f3
root <root@DESKTOP-K91FGTS>
2024-07-10 16:26:06 -0700

    Modified Welcome page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// pages/index.js
+//import Head from 'next/head';
+//import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>WELCOME!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt tortor aliquam nulla facilisi. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Dignissim enim sit amet venenatis urna cursus eget. Eget est lorem ipsum dolor. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Arcu dictum varius duis at consectetur lorem donec massa sapien. Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Nec dui nunc mattis enim ut tellus elementum. Ac placerat vestibulum lectus mauris ultrices eros. Quis auctor elit sed vulputate mi sit. Arcu cursus euismod quis viverra. At augue eget arcu dictum. Felis donec et odio pellentesque diam volutpat. Tortor posuere ac ut consequat semper viverra. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Quis lectus nulla at volutpat diam ut venenatis tellus. Urna condimentum mattis pellentesque id nibh tortor.</p>
-      </div>
-    </main>
+    <div className={styles.container}>
+      
+
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+         
+          <div className={styles.authButtons}>
+            <button className={styles.loginButton}>Login</button>
+            <button className={styles.signupButton}>Sign up</button>
+          </div>
+        </nav>
+      </header>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>WELCOME!</h1>
+        <p className={styles.description}>
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+        </p>
+        <div className={styles.imagePlaceholder}>
+          {/* <img src="/placeholder.png" alt="Placeholder" /> */}
+        </div>
+        <div className={styles.contactSection}>
+          <button className={styles.contactButton}>Shop by form</button>
+          <p>OR</p>
+          <p>Call us xxx-xxx-xxxx</p>
+        </div>
+      </main>
+    </div>
   );
 }
